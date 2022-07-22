4b5ef78124e597a2dfe25dd9ee2cb2e7809a1948
acidf0x <ic_jdh@me.com>
2022-07-22 13:58:22 +0900

    chore - project init

---
+++ src/main/kotlin/io/github/acidfox/kopringbootauthapi/KopringBootAuthApiApplication.kt
@@ -0,0 +1,11 @@
+package io.github.acidfox.kopringbootauthapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KopringBootAuthApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<KopringBootAuthApiApplication>(*args)
+}
---
+++ src/test/kotlin/io/github/acidfox/kopringbootauthapi/KopringBootAuthApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.acidfox.kopringbootauthapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KopringBootAuthApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
