7ddc036729f3dd7ad3f9652e31658c4ea9543654
acidf0x <ic_jdh@me.com>
2022-07-24 01:47:33 +0900

    feat - authcode controller - issue api

---
+++ src/main/kotlin/io/github/acidfox/kopringbootauthapi/userinterface/controller/AuthCodeController.kt
@@ -0,0 +1,24 @@
+package io.github.acidfox.kopringbootauthapi.userinterface.controller
+
+import io.github.acidfox.kopringbootauthapi.application.service.AuthCodeService
+import io.github.acidfox.kopringbootauthapi.domain.authcode.enum.AuthCodeType
+import io.github.acidfox.kopringbootauthapi.application.request.SignUpAuthCodeIssueRequest
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/auth/auth-code")
+class AuthCodeController(
+    private val authCodeService: AuthCodeService
+) {
+    @PostMapping("/sign-up")
+    fun issueSignUpAuthCode(@RequestBody @Validated request: SignUpAuthCodeIssueRequest): String {
+        // TODO : 발급 시간과 유효 기간 리턴 해주도록 변경
+        authCodeService.issue(request.phoneNumber, AuthCodeType.SIGN_UP)
+        return ResponseEntity.ok().body("").toString()
+    }
+}
