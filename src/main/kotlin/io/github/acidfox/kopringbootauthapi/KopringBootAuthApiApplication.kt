6b321ee5ac3d5a4f9b1b62b4d1ddc6f636900fc4
acidf0x <ic_jdh@me.com>
2022-07-23 11:58:06 +0900

    chore - add jpa/h2 setting for kotlin

--- src/main/kotlin/io/github/acidfox/kopringbootauthapi/KopringBootAuthApiApplication.kt
+++ src/main/kotlin/io/github/acidfox/kopringbootauthapi/KopringBootAuthApiApplication.kt
@@ -2,7 +2,9 @@ package io.github.acidfox.kopringbootauthapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
+@EnableJpaAuditing
 @SpringBootApplication
 class KopringBootAuthApiApplication
 
---
+++ src/main/kotlin/io/github/acidfox/kopringbootauthapi/domain/model/BaseEntity.kt
@@ -0,0 +1,28 @@
+package io.github.acidfox.kopringbootauthapi.domain.model
+
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+import javax.persistence.Column
+import javax.persistence.EntityListeners
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.MappedSuperclass
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null
+
+    @CreatedDate
+    @Column(nullable = false)
+    lateinit var createdAt: LocalDateTime
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    lateinit var updatedAt: LocalDateTime
+}
