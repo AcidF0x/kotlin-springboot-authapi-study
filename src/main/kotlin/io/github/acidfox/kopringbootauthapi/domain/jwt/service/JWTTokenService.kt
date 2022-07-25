c14f478a68b2170eec9e6c98de8c9cce8069f616
acidf0x <ic_jdh@me.com>
2022-07-25 12:51:31 +0900

    feat - JWT Token Service - createJWTToken

---
+++ src/main/kotlin/io/github/acidfox/kopringbootauthapi/domain/jwt/service/JWTTokenService.kt
@@ -0,0 +1,36 @@
+package io.github.acidfox.kopringbootauthapi.domain.jwt.service
+
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+import java.sql.Timestamp
+import java.time.LocalDateTime
+import java.util.Base64
+import javax.annotation.PostConstruct
+
+@Service
+class JWTTokenService(
+    @Value("\${config.auth.jwt.token-secret-key}")
+    var jwtSecretKey: String,
+    @Value("\${config.auth.jwt.token-life-time-seconds}")
+    var jwtTokenLifeTime: Int
+) {
+
+    @PostConstruct
+    private fun encodeSecretKey() {
+        jwtSecretKey = Base64.getEncoder().encodeToString(jwtSecretKey.toByteArray())
+    }
+
+    fun createJWTToken(phoneNumber: String): String {
+        val now = LocalDateTime.now()
+        val claims = Jwts.claims().setSubject(phoneNumber)
+
+        return Jwts.builder()
+            .setClaims(claims)
+            .setIssuedAt(Timestamp.valueOf(now))
+            .setExpiration(Timestamp.valueOf(now.plusSeconds(jwtTokenLifeTime.toLong())))
+            .signWith(SignatureAlgorithm.HS256, jwtSecretKey)
+            .compact()
+    }
+}
---
+++ src/test/kotlin/io/github/acidfox/kopringbootauthapi/domain/jwt/service/JWTTokenServiceTest.kt
@@ -0,0 +1,34 @@
+package io.github.acidfox.kopringbootauthapi.domain.jwt.service
+
+import io.jsonwebtoken.Jwts
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@ActiveProfiles("test")
+@SpringBootTest
+internal class JWTTokenServiceTest {
+    @Autowired
+    lateinit var jwtTokenService: JWTTokenService
+
+    @Test
+    @DisplayName("휴대전화 번호로 JWT 토큰을 발급 받을 수 있다")
+    fun testCreateJWTToken() {
+        // Given
+        val phoneNumber = "01012341234"
+
+        // When
+        val result = jwtTokenService.createJWTToken(phoneNumber)
+
+        // Then
+        val token = Jwts.parser()
+            .setSigningKey(jwtTokenService.jwtSecretKey)
+            .parseClaimsJws(result)
+            .body
+
+        Assertions.assertEquals(phoneNumber, token.subject)
+    }
+}
