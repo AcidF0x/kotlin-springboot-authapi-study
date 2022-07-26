d4bb4536c279f8a7d6626400586660deb8aee90e
acidf0x <ic_jdh@me.com>
2022-07-26 14:09:34 +0900

    refact - remove unused interface

--- src/main/kotlin/io/github/acidfox/kopringbootauthapi/application/response/LoginResponse.kt
+++ src/main/kotlin/io/github/acidfox/kopringbootauthapi/application/response/LoginResponse.kt
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 data class LoginResponse(
     val token: String
-) : Responsible {
+) {
     @JsonProperty("token_type")
     val tokenType = "Bearer"
 }
