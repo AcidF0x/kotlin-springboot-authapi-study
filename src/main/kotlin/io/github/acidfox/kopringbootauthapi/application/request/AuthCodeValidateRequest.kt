a90bde5e6e4f505c51e9e18b57d025325b7a2128
acidf0x <ic_jdh@me.com>
2022-07-26 17:24:06 +0900

    feat - validate passwrod reset authcode api

--- src/main/kotlin/io/github/acidfox/kopringbootauthapi/application/request/AuthCodeValidateRequest.kt
+++ src/main/kotlin/io/github/acidfox/kopringbootauthapi/application/request/AuthCodeValidateRequest.kt
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.Pattern
 
-data class SignUpAuthCodeValidateRequest(
+data class AuthCodeValidateRequest(
     @field: Pattern(regexp = "^01([0|1|6|7|8|9])([0-9]{7,8})$", message = "휴대전화 번호를 확인해주세요")
     @JsonProperty("phone_number")
     val phoneNumber: String,
