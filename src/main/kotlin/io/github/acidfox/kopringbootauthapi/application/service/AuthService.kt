4df52ca4e247de61ceaadb92ab0ff707a13565c2
acidf0x <ic_jdh@me.com>
2022-07-26 18:04:42 +0900

    feat - auth service - password change

--- src/main/kotlin/io/github/acidfox/kopringbootauthapi/application/service/AuthService.kt
+++ src/main/kotlin/io/github/acidfox/kopringbootauthapi/application/service/AuthService.kt
@@ -1,6 +1,7 @@
 package io.github.acidfox.kopringbootauthapi.application.service
 
 import io.github.acidfox.kopringbootauthapi.application.request.LoginRequest
+import io.github.acidfox.kopringbootauthapi.application.request.PasswordChangeRequest
 import io.github.acidfox.kopringbootauthapi.application.request.SignUpRequest
 import io.github.acidfox.kopringbootauthapi.application.response.LoginResponse
 import io.github.acidfox.kopringbootauthapi.domain.authcode.enum.AuthCodeType
@@ -8,6 +9,7 @@ import io.github.acidfox.kopringbootauthapi.domain.authcode.service.AuthCodeDoma
 import io.github.acidfox.kopringbootauthapi.domain.jwt.service.JWTTokenService
 import io.github.acidfox.kopringbootauthapi.domain.user.exception.UserNotFoundException
 import io.github.acidfox.kopringbootauthapi.domain.user.service.UserDomainService
+import org.flywaydb.core.internal.configuration.ConfigUtils.PASSWORD
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -34,5 +36,17 @@ class AuthService(
         }
 
         return LoginResponse(jwtTokenService.createJWTToken(user.email, user.passwordChangedAt))
+    }
+
+    @Transactional
+    fun passwordChanged(requestDto: PasswordChangeRequest) {
+        val user = userDomainService.findByEmailAndPhoneNumber(requestDto.email, requestDto.phoneNumber)
+
+        if (user === null) {
+            throw UserNotFoundException("사용자를 찾을 수 없습니다, 이메일 또는 휴대 전화 번호를 확인 해 주세요")
+        }
+
+        authCodeDomainService.verifyValidation(requestDto.phoneNumber, AuthCodeType.RESET_PASSWORD)
+        userDomainService.changePassword(user, requestDto.password)
     }
 }
